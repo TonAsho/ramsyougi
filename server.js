52247a267a9f42f710df9c5fd0efda0ccc56fcb0
RamSeiya <tonasho2140@gmail.com>
2022-02-08 21:09:20 +0900

    外観をよくする

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 let express = require('express');
+let ejs = require("ejs");
 let app = express();
 
 app.use(express.static("public"));
 app.use(express.static("views"));
-
+app.engine('ejs',ejs.renderFile);
 app.get("/", (req, res) => {
-    res.render("index.html");
+    res.render("home.ejs");
 });
-
+app.get("/game", (req, res) => {
+    res.render("game.ejs");
+});
 app.listen(3000, () => {
     console.log("Node.js server is running...");
 });
