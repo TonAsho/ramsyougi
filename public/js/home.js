c110334ad57157ee1e3a33f651b04e1339484593
= <tonasho2140@gmail.com>
2022-02-10 18:34:31 +0900

    design

---
+++ public/js/home.js
@@ -0,0 +1,27 @@
+console.log("Hello World!");
+let socket = io();
+window.onload = function() {
+    setUp();
+}
+function setUp() {
+    document.getElementById("start").addEventListener("click", () => {
+        socket.emit("waiting", "")
+        document.getElementById("main").style.display = "none";
+        document.getElementById("wait").style.display = "block";
+    })
+    document.getElementById("back").addEventListener("click", () => {
+        socket.emit("cancelWaiting", "")
+        document.getElementById("main").style.display = "block";
+        document.getElementById("wait").style.display = "none";
+    })
+}
+
+socket.on("hello", (message) => {
+    console.log(message);
+})
+socket.on("userCount", (count) => {
+    document.getElementById("userCount").innerHTML = `${count}人`;
+})
+socket.on("m", (m) => {
+    console.log(m)
+})
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 let express = require('express');
-let ejs = require("ejs");
 let app = express();
+let server = require("http").createServer(app);
+let io = require("socket.io")(server);
+let ejs = require("ejs");
 
 app.use(express.static("public"));
 app.use(express.static("views"));
@@ -11,6 +13,53 @@ app.get("/", (req, res) => {
 app.get("/game", (req, res) => {
     res.render("game.ejs");
 });
-app.listen(3000, () => {
+
+let waitingUserCount = 0;
+let waitings = [];
+let interval = setInterval(function() {
+    while(waitings.length > 1) {
+        let n = Math.floor(Math.random() * waitings.length);
+        let firstPerson = waitings[n];
+        waitings.splice(n, 1);
+        let n2 = Math.floor(Math.random() * waitings.length);
+        let secondPerson = waitings[n2];
+        waitings.splice(n, 1);
+        firstPerson.join(`${firstPerson.id}${secondPerson.id}`);
+        secondPerson.join(`${firstPerson.id}${secondPerson.id}`);
+        io.to(`${firstPerson.id}${secondPerson.id}`).emit("m","joined to room");
+        waitingUserCount-=2;
+    }
+    console.log(waitings.length)
+}, 10000);
+
+io.on("connection", function(socket) {
+    socket.on("waiting", () => {
+        waitingUserCount++;
+        waitings.push(socket);
+        io.sockets.emit("userCount", waitingUserCount);
+    })
+    socket.on("cancelWaiting", () => {
+        console.log(waitings.length)
+        deleteNumber(socket);
+        console.log(waitings.length)
+        waitingUserCount--;
+        io.sockets.emit("userCount", waitingUserCount);
+    })
+    socket.emit("hello", "うんこ");
+    socket.on("disconnect", () => {
+        deleteNumber(socket);
+        waitingUserCount--;
+        io.sockets.emit("userCount", waitingUserCount);
+    })
+})
+function deleteNumber(so) {
+    for(let n=0;n<waitings.length;n++) {
+        if(so === waitings[n]) {
+            waitings.slice(n,1);
+            n = waitings.length+1;
+        }
+    }
+}
+server.listen(3000, () => {
     console.log("Node.js server is running...");
 });
